e4874ac608b859991b1f627190d0361816a47097
Alois Kabelschacht <andrii.verbytskyi@mpp.mpg.de>
2021-12-29 10:48:12 +0100

    Reorder dirs

--- 3.2.5/setup.py
+++ 3.2.5/setup.py
@@ -292,6 +292,8 @@ class hm3_build_ext(build_ext_orig):
         if not self.dry_run:
             build_args = []
             self.spawn([cmake_exe, "--build", "."] + build_args)
+            print(os.listdir("outputs"))
+            print(os.listdir("outputs/lib"))
             ctest_args = []
             v = sys.version_info
             if ps == "Windows":
